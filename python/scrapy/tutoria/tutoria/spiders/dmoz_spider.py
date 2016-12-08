b0b6178e9ccbaf17d949c3c206e3cafda73539e7
zhg5482 <1581992170@qq.com>
2016-12-08 16:55:07 +0800

    update

---
+++ python/scrapy/tutoria/tutoria/spiders/dmoz_spider.py
@@ -0,0 +1,20 @@
+import scrapy
+
+class DmozSpider(scrapy.Spider):
+	name = "dmoz"
+	allowed_domains = ["dmoz.org"]
+	start_url = [
+			"http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+			"http://www.dmoz.org/Computers/Programming/Languages/Python/Books/"]
+
+#	def parse(self,response):
+#		filename = response.url.split("/")[-2]
+#		with open(filename,'wb') as f:
+#			f.write(response.body)
+
+	def parse(self,reponse):
+		for sel in response.xpath('//ul/li'):
+			title = sel.xpath('a/text()').extract()
+			link = sel.xpath('a/@href').extract()
+			desc = sel.xpath('text()').extract()
+			print title,link,desc
